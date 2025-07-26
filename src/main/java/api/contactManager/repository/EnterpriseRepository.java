5d32dce5b5540e0f03b031644934ff3d21a4a3af
pierre <pierrecrochet03@gmail.com>
2025-07-26 09:46:20 +0200

    adding repository

---
+++ src/main/java/api/contactManager/repository/EnterpriseRepository.java
@@ -0,0 +1,11 @@
+package api.contactManager.repository;
+
+import api.contactManager.domain.Enterprise;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EnterpriseRepository extends JpaRepository<Enterprise, UUID> {
+}
