061a00a02285f5e7894c768d69582837ee7b3075
pierre <pierrecrochet03@gmail.com>
2025-07-27 15:07:25 +0200

    update return value of health

--- src/main/java/api/contactManager/controller/template/v1/HealthControllerV1.java
+++ src/main/java/api/contactManager/controller/template/v1/HealthControllerV1.java
@@ -4,14 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.util.Map;
 
 @Controller
 public class HealthControllerV1 extends BaseRestController {
 
     @GetMapping("/health/")
-    public ResponseEntity<String> health() {
+    public ResponseEntity<Map<String,String>> health() {
         return ResponseEntity
-                .status(HttpStatus.OK).body("Ok");
+                .status(HttpStatus.OK).body(Map.of("message", "Ok"));
     }
 
 }
