25e752a5ee06461ba4d22b043f57ffdfa45bf79b
pierre <pierrecrochet03@gmail.com>
2025-07-28 19:28:35 +0200

    chore unused imports

--- src/main/java/api/contactManager/controller/template/v1/EnterpriseControllerV1.java
+++ src/main/java/api/contactManager/controller/template/v1/EnterpriseControllerV1.java
@@ -1,13 +1,11 @@
 package api.contactManager.controller.template.v1;
 
-
 import api.contactManager.dto.EnterpriseDTO;
 import api.contactManager.service.EnterpriseService;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
